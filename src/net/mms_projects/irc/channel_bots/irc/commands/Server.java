2065b23526d3adc2c8c4a0e385fa94c9513e63f2
Marlin Cremers <m.cremers@mms-projects.net>
2012-12-01 16:57:17 +0100

    Fixed the server command

--- src/net/mms_projects/irc/channel_bots/irc/commands/Server.java
+++ src/net/mms_projects/irc/channel_bots/irc/commands/Server.java
@@ -1,50 +1,52 @@
 package net.mms_projects.irc.channel_bots.irc.commands;
 
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import net.mms_projects.irc.channel_bots.irc.Command;
 
 public class Server extends Command {
 
-	public String name;
-	public int hopcount;
-	public String protocolVersion;
-	public String protocolFlags;
-	public String serverNumeric;
 	public String server;
+	public int hopCount;
 	public String description;
 	
+	// Optional options
+	public int protocolVersion;
+	public String protocolFlags;
+	public int serverNumeric;
+	public String serverName;
+	
 	public Server() {
-		//SERVER server.name 1 :Uprotocolversion-protocolflags-servernumeric server description
-		//SERVER dev.mms-projects.net 1 :U2310-FhinXOoE-100 MMS-Projects Development server
-		super("SERVER (.+) (\\d+) :(.+)-(.+)-(.+) (.+) (.+)", "");
+		super("SERVER (.+) (\\d+) :(.+)", "SERVER %s %d :%s");
 	}
 
 	@Override
 	public void parse(String rawdata) {
 		Matcher matcher = this.match(rawdata);
-		System.out.println("BLABLABLA" + rawdata);
 		if (matcher.find()) {
-			this.name = matcher.group(1);
-			this.hopcount = Integer.parseInt(matcher.group(2));
-			this.protocolVersion = matcher.group(3);
-			this.protocolFlags = matcher.group(4);
-			this.serverNumeric = matcher.group(5);
-			this.server = matcher.group(6);
-			this.description = matcher.group(7);
-			System.out.println("Name: " + name);
-			System.out.println("Hopcount: " + hopcount);
-			System.out.println("proVersion: " + protocolVersion);
-			System.out.println("proFlags: " + protocolFlags);
-			System.out.println("serverNumeric: " + serverNumeric);
-			System.out.println("Server: " + server);
-			System.out.println("Description: " + description);
+			this.server = matcher.group(1);
+			this.hopCount = Integer.parseInt(matcher.group(2));
+			this.description = matcher.group(3);
+			
+			Pattern extraInfo = Pattern.compile("U(\\d*)-([a-zA-Z]*)-(\\d*) (.+)");
+			Matcher extraInfoMatcher = extraInfo.matcher(this.description);
+			if (extraInfoMatcher.find()) {
+				this.protocolVersion = Integer.parseInt(extraInfoMatcher.group(1));
+				this.protocolFlags = extraInfoMatcher.group(2);
+				this.serverNumeric = Integer.parseInt(extraInfoMatcher.group(3));
+				this.serverName = extraInfoMatcher.group(4);
+			}
 		}
 	}
 
 	@Override
 	public String build() {
-		return "";
+		String description = this.description;
+		if (this.protocolFlags.length() > 0) {
+			description = String.format("U%d-%s-%d %s", this.protocolVersion, this.protocolFlags, this.serverNumeric, this.serverName);
+		}
+		return String.format(this.outputPattern, this.server, this.hopCount, description);
 	}
 
 }
