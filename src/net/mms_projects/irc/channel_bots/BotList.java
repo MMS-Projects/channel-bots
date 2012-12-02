a111e6f277fb40fbc8fe99c40f12751f4e3872ac
Sietse <sietse97@gmail.com>
2012-12-02 12:42:53 +0100

    Created a basic BotList.

---
+++ src/net/mms_projects/irc/channel_bots/BotList.java
@@ -0,0 +1,8 @@
+package net.mms_projects.irc.channel_bots;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@SuppressWarnings("serial")
+public class BotList extends CopyOnWriteArrayList<Bot> {
+
+}
