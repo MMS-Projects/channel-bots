0dbc8ae0bbc7a2de0c608f75cce9ee7ea07a8bbf
Sietse <sietse97@gmail.com>
2012-12-02 13:06:41 +0100

    ProgrammableBots now uses the ServiceManager.

--- src/net/mms_projects/irc/channel_bots/plugins/ProgrammableBots.java
+++ src/net/mms_projects/irc/channel_bots/plugins/ProgrammableBots.java
@@ -1,13 +1,12 @@
 package net.mms_projects.irc.channel_bots.plugins;
 
-import net.mms_projects.irc.channel_bots.ChannelBots;
+import net.mms_projects.irc.channel_bots.Bot;
 import net.mms_projects.irc.channel_bots.ChannelList;
 import net.mms_projects.irc.channel_bots.Plugin;
 import net.mms_projects.irc.channel_bots.ServerList;
 import net.mms_projects.irc.channel_bots.Socket;
 import net.mms_projects.irc.channel_bots.UserList;
 import net.mms_projects.irc.channel_bots.irc.Handler;
-import net.mms_projects.irc.channel_bots.irc.commands.NickIntroduce;
 import net.mms_projects.irc.channel_bots.managers.ServiceManager;
 
 public class ProgrammableBots extends Plugin {
@@ -20,16 +19,8 @@ public class ProgrammableBots extends Plugin {
 		this.manager = new ServiceManager(socket, handler, userList, channelList, serverList);
 		handler.addManager(this.manager);
 		
-		NickIntroduce pBot = new NickIntroduce();
-		pBot.nickname = "PBot";
-		pBot.hobs = 1;
-		pBot.timestamp = (int) (ChannelBots.date.getDate().getTime() / 1000);
-		pBot.username = "P-Bot";
-		pBot.hostname = "channel-bot.mms-projects.net";
-		pBot.server = "channels.mms-projects.net";
-		pBot.serviceStamp = 1;
-		pBot.realname = "Programmable Bot interface";
-		socket.write(pBot.toString());
+		this.manager.newBot(new Bot("PBot", "Programmable Bot interface",
+				"P-Bot", "channel-bot.mms-projects.net"));
 	}
 
 }
