606a2549f7ad6fdde009df0b975956ae9f52a8d5
Marlin Cremers <m.cremers@mms-projects.net>
2012-12-02 13:16:58 +0100

    Added privmsg and notice to the bot class

--- src/net/mms_projects/irc/channel_bots/Bot.java
+++ src/net/mms_projects/irc/channel_bots/Bot.java
@@ -2,6 +2,8 @@ package net.mms_projects.irc.channel_bots;
 
 import net.mms_projects.irc.channel_bots.irc.Command;
 import net.mms_projects.irc.channel_bots.irc.commands.NickIntroduce;
+import net.mms_projects.irc.channel_bots.irc.commands.Notice;
+import net.mms_projects.irc.channel_bots.irc.commands.PrivMsg;
 import net.mms_projects.irc.channel_bots.irc.commands.Quit;
 import net.mms_projects.irc.channel_bots.managers.ServiceManager;
 
@@ -12,8 +14,8 @@ public class Bot {
 	public String realname;
 	public ServiceManager manager;
 	public boolean quit;
-	
-	public Bot (String nickname, String realname, String username, 
+
+	public Bot(String nickname, String realname, String username,
 			String hostname) {
 		this.quit = false;
 		this.nickname = nickname;
@@ -34,20 +36,37 @@ public class Bot {
 		c.realname = realname;
 		return c;
 	}
-	
-	public void write (Command command) {
+
+	public void write(Command command) {
 		manager.write(command);
 	}
-	
-	public void quit (String reason) {
+
+	public void quit(String reason) {
 		Quit c = new Quit();
 		c.nickname = nickname;
 		c.reason = reason;
 		write(c);
 		quit = true;
 	}
-	
-	public void join (String channel) {
-		
+
+	public void join(String channel) {
+
 	}
+	
+	public void privMSg(String target, String text) {
+		PrivMsg privMsg = new PrivMsg();
+		privMsg.source = this.nickname;
+		privMsg.target = target;
+		privMsg.text = text;
+		write(privMsg);
+	}
+	
+	public void notice(String target, String text) {
+		Notice notice = new Notice();
+		notice.source = this.nickname;
+		notice.target = target;
+		notice.text = text;
+		write(notice);
+	}
+	
 }
