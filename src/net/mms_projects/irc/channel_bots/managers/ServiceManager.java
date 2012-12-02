160f3af81df8e47aae720737509ea5d88ee0c410
Sietse <sietse97@gmail.com>
2012-12-02 12:38:48 +0100

    Created a basic service manager.

---
+++ src/net/mms_projects/irc/channel_bots/managers/ServiceManager.java
@@ -0,0 +1,11 @@
+package net.mms_projects.irc.channel_bots.managers;
+
+import net.mms_projects.irc.channel_bots.Manager;
+
+public class ServiceManager extends Manager {
+
+	@Override
+	public void tick() {
+		
+	}
+}
