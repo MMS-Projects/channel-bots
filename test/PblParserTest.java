90648d3af95795960d28793dc029ab8101108fa5
Marlin Cremers <m.cremers@mms-projects.net>
2012-12-04 23:32:20 +0100

    Added a simple unit test for the PBL parser

---
+++ test/PblParserTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+
+import net.mms_projects.irc.channel_bots.pbl.Handler;
+import net.mms_projects.irc.channel_bots.pbl.Parser;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PblParserTest {
+
+	private static Parser parser;
+	
+	@BeforeClass
+	public static void testSetup() {
+		Handler handler = new Handler();
+		parser = new Parser(handler);
+	}
+
+	@AfterClass
+	public static void testCleanup() {
+		parser = null;
+	}
+
+	@Test
+	public void testParse() {
+		parseTest("test2", "$replace(test1, test1, test2)");
+		parseTest("65", "$asc(A)");
+		parseTest("A", "$chr(65)");
+		//parseTest("65", "$asc($replace(B, B, A))");
+		parseTest("Test value?", "%internal.testvariable");
+		parseTest("Leuke value?", "$replace(%internal.testvariable, Test, Leuke)");
+		
+	}
+	
+	private void parseTest(String expected, String input) {
+		assertEquals(expected, parser.parse(input));
+	}
+
+}
