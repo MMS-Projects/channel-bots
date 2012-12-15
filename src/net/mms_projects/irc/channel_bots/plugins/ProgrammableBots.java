b01379be5ca658d61bb49dde73500219283b0e13
Marlin Cremers <m.cremers@mms-projects.net>
2012-12-15 13:38:28 +0100

    Triggers get pulled from the database now

--- src/net/mms_projects/irc/channel_bots/plugins/ProgrammableBots.java
+++ src/net/mms_projects/irc/channel_bots/plugins/ProgrammableBots.java
@@ -4,8 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.slf4j.LoggerFactory;
+
+import com.jolbox.bonecp.BoneCP;
+import com.jolbox.bonecp.BoneCPConfig;
 
 import net.mms_projects.irc.channel_bots.Bot;
+import net.mms_projects.irc.channel_bots.Channel;
 import net.mms_projects.irc.channel_bots.ChannelList;
 import net.mms_projects.irc.channel_bots.Plugin;
 import net.mms_projects.irc.channel_bots.ServerList;
@@ -18,9 +27,11 @@ import net.mms_projects.irc.channel_bots.listeners.MessageListener;
 import net.mms_projects.irc.channel_bots.managers.ServiceManager;
 import net.mms_projects.irc.channel_bots.pb.CommandHandler;
 import net.mms_projects.irc.channel_bots.pb.PassedData;
+import net.mms_projects.irc.channel_bots.pb.Trigger;
 import net.mms_projects.irc.channel_bots.pb.commands.Add;
 import net.mms_projects.irc.channel_bots.pb.commands.Help;
 import net.mms_projects.irc.channel_bots.pb.commands.Variable;
+import net.mms_projects.irc.channel_bots.pb.tables.Triggers;
 import net.mms_projects.irc.channel_bots.pbl.Parser;
 
 public class ProgrammableBots extends Plugin implements MessageListener {
@@ -33,18 +44,20 @@ public class ProgrammableBots extends Plugin implements MessageListener {
 	ResultSet resultSet = null;
 	Statement statement = null;
 	private CommandHandler cmdHandler;
+	private Triggers triggerTable;
 
 	public ProgrammableBots(Socket socket, Handler handler, UserList userList,
 			ChannelList channelList, ServerList serverList) {
 		super(socket, handler, userList, channelList, serverList);
 
+		Logger.getGlobal().setLevel(Level.ALL);
+		
 		this.pblHandler = new net.mms_projects.irc.channel_bots.pbl.Handler();
 		this.pblParser = new Parser(this.pblHandler);
 
 		this.manager = new ServiceManager(socket, handler, userList,
 				channelList, serverList);
-		handler.addManager(this.manager);
-		handler.addMessageListener(this);
+		
 
 		pbot = new Bot("PBot", "Programmable Bot interface", "P-Bot",
 				"channel-bot.mms-projects.net");
@@ -60,26 +73,30 @@ public class ProgrammableBots extends Plugin implements MessageListener {
 
 		try {
 			Class.forName("org.sqlite.JDBC");
-			connection = DriverManager
-					.getConnection("jdbc:sqlite:data/data.db");
-			statement = connection.createStatement();
-			resultSet = statement.executeQuery("SELECT * FROM pb_triggers");
-			while (resultSet.next()) {
-				System.out.println("Trigger data:"
-						+ resultSet.getString("data"));
-			}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally {
-			try {
-				resultSet.close();
-				statement.close();
-				connection.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
 		}
+		
+		BoneCP connectionPool = null;
+		
+		try {
+			// setup the connection pool
+			BoneCPConfig config = new BoneCPConfig();
+			config.setJdbcUrl("jdbc:sqlite:data/data.db");
+			config.setMinConnectionsPerPartition(5);
+			config.setMaxConnectionsPerPartition(10);
+			config.setPartitionCount(1);
+			config.setUsername("");
+			config.setPassword("");
+			connectionPool = new BoneCP(config);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		triggerTable = new Triggers(connectionPool);
+		
+		handler.addManager(this.manager);
+		handler.addMessageListener(this);
 	}
 
 	public void tick() {
@@ -89,10 +106,18 @@ public class ProgrammableBots extends Plugin implements MessageListener {
 	@Override
 	public void onPrivMsg(PrivMsg event) {
 		if (event.target.equalsIgnoreCase("#test")) {
+			Channel channel = this.channelList.getChannelByName(event.target);
 			this.pblHandler.setVariable("internal.irc.chan", event.target);
 			this.pblHandler.setVariable("internal.irc.nick", event.source);
 
 			this.pbot.privMsg(event.target, this.pblParser.eval(event.text));
+			
+			List<Trigger> triggers = this.triggerTable.getChannelTriggers(channel);
+			for (Trigger trigger : triggers) {
+				if (trigger.matches(event)) {
+					this.pbot.privMsg(event.target, trigger.id + ": " + trigger.data);
+				}
+			}
 		}
 		if (event.target.equalsIgnoreCase(this.pbot.nickname)) {
 			boolean handled = this.cmdHandler
