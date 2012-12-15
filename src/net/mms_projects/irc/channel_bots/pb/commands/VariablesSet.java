6edcc9d16e4409ccddd3667560a24ecccf0a3e7b
Marlin Cremers <m.cremers@mms-projects.net>
2012-12-15 21:25:27 +0100

    Renamed all the PBot variable commands to variables

--- src/net/mms_projects/irc/channel_bots/pb/commands/VariablesSet.java
+++ src/net/mms_projects/irc/channel_bots/pb/commands/VariablesSet.java
@@ -7,9 +7,9 @@ import net.mms_projects.irc.channel_bots.pb.CommandSyntax;
 import net.mms_projects.irc.channel_bots.pb.PassedData;
 import net.mms_projects.irc.channel_bots.pb.command_arguments.Text;
 
-public class VariableSet extends Command {
+public class VariablesSet extends Command {
 
-	public VariableSet(CommandHandler handler, Command parent) {
+	public VariablesSet(CommandHandler handler, Command parent) {
 		super("set", "Used to set a variable", handler, parent);
 
 		this.setLongDescription("This sets a channel variable. If a "
